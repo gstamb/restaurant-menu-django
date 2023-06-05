63ef152675d106ef9cc15fc81747b6314bfffcaa
gstamb <georgi.stamboliyski@university.mentormate.com>
2023-06-05 15:38:09 +0200

    Add dynamic links

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='')
+    path('', views.MenuList.as_view(), name=''),
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name='menu_item')
 
 ]
-
