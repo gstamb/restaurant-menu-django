fe30bb9966f19ef473950e23ae085864ed90303b
gstamb <georgi.stamboliyski@university.mentormate.com>
2023-06-05 13:25:02 +0200

    Migrate database model

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salads", "Salads"),
+    ("main_dishes", "Main_dishes"),
+    ("deserts", "Deserts")
+)
+
+STATUS = (
+    (0, "Unavailable"),
+    (1, "Available")
+)
+
+
+class Item(models.Model):
+    meal = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
+    # CASCADE OR PROTECT OR SET_NULL If you want to behave differently upon author deletion
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=0)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_updates = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal
